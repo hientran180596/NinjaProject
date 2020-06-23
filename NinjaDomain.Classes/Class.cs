28bc4efa45f23e7b65bb951ce77e2c0b8d1ea844
hientran180596 <hienronaldo16@gmail.com>
2020-06-23 22:56:45 +0700

    Upsource

---
+++ NinjaDomain.Classes/Class.cs
@@ -0,0 +1,38 @@
+﻿using NinjaDomain.Classes.Enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NinjaDomain.Classes
+{
+    public class Class
+    {
+        public class Ninja
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public bool ServerInOniwaban { get; set; }
+            public Clan Clan { get; set; }
+            public int ClanId { get; set; }
+            public List<NinjaEquipment> EquipmentOwned { get; set; }
+        }
+        public class Clan
+        {
+            public int Id { get; set; }
+            public string ClanName { get; set; }
+            public List<Ninja> Ninjas { get; set; }
+        }
+        public class NinjaEquipment
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public EquipmentType Type { get; set; }
+            [Required]
+            public Ninja Ninja { get; set; }
+        }
+    }
+}
